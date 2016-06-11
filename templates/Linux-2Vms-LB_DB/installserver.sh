8fcd9c7f02f62ed9a3c04e46387b6eb0a4679eac
Hatem AlSum <htotisum@gmail.com>
2016-06-11 14:45:12 +0200

    fix extenstion

--- templates/Linux-2Vms-LB_DB/installserver.sh
+++ templates/Linux-2Vms-LB_DB/installserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install apache service 
 yum -y update
-yum -y install epel-release 
-yum -y install httpd php php-mysql php-mbstring ImageMagick php-mcrypt 
+yum -y install httpd php php-mysql 
 setsebool -P httpd_can_network_connect=1 
 service httpd start
+chkconfig httpd on
