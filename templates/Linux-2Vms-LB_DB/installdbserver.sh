ffab27152dc625f68063d6e0783c85f80e4f9f4b
Hatem AlSum <htotisum@gmail.com>
2016-06-17 17:48:44 +0200

    Add EPEL Repos

--- templates/Linux-2Vms-LB_DB/installdbserver.sh
+++ templates/Linux-2Vms-LB_DB/installdbserver.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 yum -y update
 # Install PhpMyAdmin
 yum -y install phpmyadmin 
--- templates/Linux-2Vms-LB_DB/installserver.sh
+++ templates/Linux-2Vms-LB_DB/installserver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # install apache service 
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 yum -y update
 yum -y install httpd php php-mysql 
 setsebool -P httpd_can_network_connect=1 
